919b2872e4e8ab3b6cfc03e4378aecb20beac17d
Tiav Randria <tiav.randria@gmail.com>
2022-02-03 09:58:39 +0100

    snapface

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,22 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
   title!: string;
   description!: string;
   createdDate!: Date;
   snaps!: number;
   imageUrl!:string
+  buttonText!: string;
 
   ngOnInit() {
-    this.title = "Archibald";
-    this.description = "Mon meilleur ami depuis tout petit !"
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.buttonText = "Oh Snap!";
+  }
+  
+  onAddSnap() {
+    this.snaps = this.snaps + 1;
+  }
+
+  onSnap() {
+    if(this.buttonText === "Oh Snap!") {
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oops, un Snap!';
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'Oh Snap!'
+    }
   }
 }
